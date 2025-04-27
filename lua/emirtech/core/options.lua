b050375f18349f66b50f23c9189896a6c68027cd
Emir Kovacevic <emir.kovacevic@misija.io>
2025-04-27 20:42:49 +0200

    update

--- lua/emirtech/core/options.lua
+++ lua/emirtech/core/options.lua
@@ -20,11 +20,32 @@ opt.autoindent = true
 
 opt.wrap = true
 
+-- Don't show the mode, since it's already in the status line
+opt.showmode = false
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
 
+-- Decrease update time
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+opt.timeoutlen = 300
+
 opt.cursorline = false
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- turn on termugicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal
@@ -32,12 +53,37 @@ opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
 
+-- Preview substitutions live, as you type!
+opt.inccommand = "split"
+
 -- backspace
 opt.backspace = "indent,eol,start"
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
 
 --clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+opt.confirm = true
+
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
