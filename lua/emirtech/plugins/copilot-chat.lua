30e8bc2788d790ce124519af636db060c4ccedad
Emir Kovacevic <emirkovacevic@protonmail.com>
2025-06-19 22:47:42 +0200

    update copilot chat

--- lua/emirtech/plugins/copilot-chat.lua
+++ lua/emirtech/plugins/copilot-chat.lua
@@ -5,26 +5,27 @@ return {
 		{ "nvim-lua/plenary.nvim", branch = "master" },
 	},
 	build = "make tiktoken", -- Optional: Only needed on macOS/Linux for token counting
-	opts = {},
-	-- Key mappings for Copilot Chat plugin actions in Neovim.
-	-- Each mapping specifies:
-	--   - key: The key combination to trigger the action.
-	--   - command: The Copilot Chat command to execute.
-	--   - mode: The editor mode ("n" for normal, "v" for visual).
-	--   - desc: A description for discoverability in which-key or similar plugins.
-	keys = (function()
-		local n = "n"
-		local v = "v"
-		return {
-			{ "<leader>zc", "<cmd>CopilotChat<CR>", mode = n, desc = "Chat with Copilot" },
-			{ "<leader>zm", "<cmd>CopilotChatCommit<CR>", mode = n, desc = "Generate Commit Message" },
-			{ "<leader>ze", "<cmd>CopilotChatExplain<CR>", mode = v, desc = "Explain Code" },
-			{ "<leader>zr", "<cmd>CopilotChatReview<CR>", mode = v, desc = "Review Code" },
-			{ "<leader>zf", "<cmd>CopilotChatFix<CR>", mode = v, desc = "Fix Code Issues" },
-			{ "<leader>zo", "<cmd>CopilotChatOptimize<CR>", mode = v, desc = "Optimize Code" },
-			{ "<leader>zd", "<cmd>CopilotChatDocs<CR>", mode = v, desc = "Generate Docs" },
-			{ "<leader>zt", "<cmd>CopilotChatTests<CR>", mode = v, desc = "Generate Tests" },
-			{ "<leader>zs", "<cmd>CopilotChatCommit<CR>", mode = v, desc = "Generate Commit for Selection" },
-		}
-	end)(),
+	opts = {
+		prompts = {
+			Rename = {
+				prompt = "Please rename the variable or function to something more descriptive and meaningful based on the provided code context.",
+				selection = function(selected_text)
+					local select_module = require("CopilotChat.select")
+					return select_module.visual(selected_text)
+				end,
+			},
+		},
+	},
+	keys = {
+		{ "<leader>zn", "<cmd>CopilotChatRename<CR>", mode = "v", desc = "Rename the variable or function" },
+		{ "<leader>zc", "<cmd>CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+		{ "<leader>ze", "<cmd>CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
+		{ "<leader>zr", "<cmd>CopilotChatReview<CR>", mode = "v", desc = "Review Code" },
+		{ "<leader>zf", "<cmd>CopilotChatFix<CR>", mode = "v", desc = "Fix Code Issues" },
+		{ "<leader>zo", "<cmd>CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
+		{ "<leader>zd", "<cmd>CopilotChatDocs<CR>", mode = "v", desc = "Generate Docs" },
+		{ "<leader>zt", "<cmd>CopilotChatTests<CR>", mode = "v", desc = "Generate Tests" },
+		{ "<leader>zm", "<cmd>CopilotChatCommit<CR>", mode = "n", desc = "Generate Commit Message" },
+		{ "<leader>zs", "<cmd>CopilotChatCommit<CR>", mode = "v", desc = "Generate Commit for Selection" },
+	},
 }
