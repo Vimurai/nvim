f1a9655b458da66fe0fd7f5e6a42409f4aad1c06
Emir Kovacevic <emirkovacevic@protonmail.com>
2025-08-21 18:45:27 +0200

    update

--- lua/emirtech/core/keymaps.lua
+++ lua/emirtech/core/keymaps.lua
@@ -6,7 +6,8 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- Disable space in normal mode so it doesn't move the cursor
 keymap.set("n", "<Space>", "<Nop>", { desc = "Disable space bar movement" })
-keymap.set("x", "<Space>", "<Nop>", { desc = "Disable space bar movement in visual mode" })keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("x", "<Space>", "<Nop>", { desc = "Disable space bar movement in visual mode" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
